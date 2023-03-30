0384bbf55544efd4ed7cd4c731d55926734f355a
AaronRioss <127362194+AaronRioss@users.noreply.github.com>
2023-03-30 20:19:05 -0300

    Delete aaron directory

--- aaron/script.js
+++
@@ -1,9 +0,0 @@
-<script>
-// Activar los botones del navbar
-$(document).ready(function() {
-  $(".navbar-nav a").on("click", function() {
-    $(".navbar-nav").find(".active").removeClass("active");
-    $(this).addClass("active");
-  });
-});
-</script>
